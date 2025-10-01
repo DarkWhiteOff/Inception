48d40a0ffef544d868f3b49090703900544d87b4
Ziggy <darkwhite.off@gmail.com>
2025-10-02 01:11:00 +0200

    brooooooooo

---
+++ Inception 2/srcs/requirements/mariadb/tools/script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+service mysql start
+
+db_name = Database Name
+db_user = User
+db_pwd = User Password
+
+db_name=nnnname
+db_user=uuuuser
+db_pwd=ppppass
+
+echo "CREATE DATABASE IF NOT EXISTS $db_name ;" > db1.sql
+echo "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd' ;" >> db1.sql
+echo "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%' ;" >> db1.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
+echo "FLUSH PRIVILEGES;" >> db1.sql
+
+mysql < db1.sql
