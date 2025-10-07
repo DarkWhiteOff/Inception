fd0be97655ac8f7440e2dad57ffb9718e85fa7d3
Ziggy Amgar <zamgar@f2r7s9.clusters.42paris.fr>
2025-10-07 15:13:18 +0200

    Inception 2 separate repo

--- Inception_2/srcs/requirements/nginx/tools/script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout \
-/etc/ssl/private/nginx-selfsigned.key -out \
-/etc/ssl/certs/nginx-selfsigned.crt -subj \
-"/C=FR/L=PA/O=42/OU=student/CN=zamgar"
-
-nginx -g "daemon off;"
