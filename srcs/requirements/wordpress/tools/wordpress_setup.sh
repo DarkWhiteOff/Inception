f84c6bafd703d9d7b22a16c24aa05e837d280589
Ziggy <darkwhite.off@gmail.com>
2025-12-10 22:33:49 +0100

    going on

--- srcs/requirements/wordpress/tools/wordpress_setup.sh
+++ srcs/requirements/wordpress/tools/wordpress_setup.sh
@@ -9,7 +9,7 @@ wp config set --allow-root DB_USER ${MARIADB_USER}
 wp config set --allow-root DB_PASSWORD ${MARIADB_PASSWORD}
 wp config set --allow-root DB_HOST mariadb:3306
 
-wp core install --allow-root --url="zamgar.42.fr" --title="My Wordpress Site" --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASS} --admin_email=${WP_ADMIN_EMAIL}
+wp core install --allow-root --url=${DOMAIN_NAME} --title="My Wordpress Site" --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASS} --admin_email=${WP_ADMIN_EMAIL}
 
 wp user create --allow-root "${WP_USER}" "${WP_EMAIL}" --user_pass=${WP_PASS} --role=author
 
