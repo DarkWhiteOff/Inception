8a50a210b91847aee5475598398ae02ca756231b
Ziggy <darkwhite.off@gmail.com>
2025-12-12 06:21:06 +0100

    wordpress changes

--- srcs/requirements/wordpress/tools/wordpress_setup.sh
+++ srcs/requirements/wordpress/tools/wordpress_setup.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
-
-sleep 10
-
-# cd /var/www/wordpress
-
-wp config set --allow-root DB_NAME ${MARIADB_DATABASE}
-wp config set --allow-root DB_USER ${MARIADB_USER}
-wp config set --allow-root DB_PASSWORD ${MARIADB_PASSWORD}
-wp config set --allow-root DB_HOST mariadb:3306
-
-wp core install --allow-root --url=${DOMAIN_NAME} --title="My Wordpress Site" --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASS} --admin_email=${WP_ADMIN_EMAIL}
-
-wp user create --allow-root "${WP_USER}" "${WP_EMAIL}" --user_pass=${WP_PASS} --role=author
+set -e
 
 mkdir -p /run/php
+chown -R www-data:www-data /run/php /var/www/wordpress
 
-echo "SERVER IS RUNNING......"
+until mysql -h mariadb -u"${MARIADB_USER}" -p"${MARIADB_PASSWORD}" \
+  -e "SELECT 1;" 1>/dev/null 2>&1; do
+  sleep 1
+done
 
-/usr/sbin/php-fpm8.2 -F
+wp config set --allow-root DB_NAME "$MARIADB_DATABASE"
+wp config set --allow-root DB_USER "$MARIADB_USER"
+wp config set --allow-root DB_PASSWORD "$MARIADB_PASSWORD"
+wp config set --allow-root DB_HOST "mariadb:3306"
 
-# exec /usr/sbin/php-fpm8.2 -F
+if ! wp core is-installed --allow-root --url="https://$DOMAIN_NAME"; then
+  wp core install --allow-root \
+    --url="$DOMAIN_NAME" \
+    --title="My WordPress Site" \
+    --admin_user="$WP_ADMIN_USER" \
+    --admin_password="$WP_ADMIN_PASS" \
+    --admin_email="$WP_ADMIN_EMAIL"
+
+  wp user create --allow-root "$WP_USER" "$WP_EMAIL" \
+    --user_pass="$WP_PASS"
+fi
+
+echo ">> SERVER IS RUNNING..."
+
+exec /usr/sbin/php-fpm8.2 -F
