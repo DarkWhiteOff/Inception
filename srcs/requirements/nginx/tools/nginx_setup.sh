8ccc1a954faf0d8490ae1d37d3b9393ee223b3aa
Ziggy <darkwhite.off@gmail.com>
2025-12-15 22:35:26 +0100

    last changes

--- srcs/requirements/nginx/tools/nginx_setup.sh
+++ srcs/requirements/nginx/tools/nginx_setup.sh
@@ -3,11 +3,9 @@ set -e
 
 mkdir -p /etc/nginx/ssl
 
-if [ ! -f /etc/nginx/ssl/inception.crt ] || [ ! -f /etc/nginx/ssl/inception.key ]; then
-    openssl req -x509 -nodes \
-        -out /etc/nginx/ssl/inception.crt \
-        -keyout /etc/nginx/ssl/inception.key \
-        -subj "/C=FR/L=PA/O=42/CN=${DOMAIN_NAME}"  
-fi
+openssl req -x509 -nodes \
+    -out /etc/nginx/ssl/inception.crt \
+    -keyout /etc/nginx/ssl/inception.key \
+    -subj "/C=FR/L=PA/O=42/CN=${DOMAIN_NAME}"  
 
 exec nginx -g 'daemon off;'
